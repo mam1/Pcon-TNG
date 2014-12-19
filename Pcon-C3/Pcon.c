0f642363e8ea4796b9e2c817c78397b78a45c451
Mark Mara <markmara@cornell.edu>
2014-12-19 11:52:25 -0500

    add fdserial_ht, clean compile

--- Pcon-C3/Pcon.c
+++ Pcon-C3/Pcon.c
@@ -9,8 +9,8 @@
 #include <stdio.h>
 #include <unistd.h>		//sleep
 #include <stdint.h>		//uint_8, uint_16, uint_32, etc.
-#include <simpletools.h>
-#include <fdserial.h>
+// #include <simpletools.h>
+// #include <fdserial.h>
 #include "Pcon.h"
 
 #define RX                      1
@@ -21,15 +21,15 @@
 #define START_COMMAND         27
 
 /*************************** drivers  ********************************/
-extern _Driver _FullDuplexSerialDriver;
-extern _Driver _FileDriver;
-_Driver *_driverlist[] = {&_FullDuplexSerialDriver,&_FileDriver,NULL};
+// extern _Driver _FullDuplexSerialDriver;
+// extern _Driver _FileDriver;
+// _Driver *_driverlist[] = {&_FullDuplexSerialDriver,&_FileDriver,NULL};
 /***************************** external ******************************/
 
 int main(int argc, char *argv[]){
 
-	fdserial			*bbb;
-	bbb =  fdserial_open(RX, TX, MODE, BAUD);
+	// fdserial			*bbb;
+//	bbb =  fdserial_open(RX, TX, MODE, BAUD);
 	sleep(1);
 	disp_sys();
 
