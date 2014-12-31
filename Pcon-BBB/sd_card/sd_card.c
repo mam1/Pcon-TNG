406d031e65e786f9a46e7df8cfa5d8818cc1e4e3
mam1 <mam1@cornell.edu>
2014-12-31 14:25:26 -0500

    adding sd card support

---
+++ Pcon-BBB/sd_card/sd_card.c
@@ -0,0 +1,13 @@
+/* 
+* @Author: mam1
+* @Date:   2014-12-31 14:13:08
+* @Last Modified by:   mam1
+* @Last Modified time: 2014-12-31 14:24:14
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
---
+++ Pcon-BBB/sd_card/test_sd_card.c
@@ -0,0 +1,13 @@
+/* 
+* @Author: mam1
+* @Date:   2014-12-31 14:15:27
+* @Last Modified by:   mam1
+* @Last Modified time: 2014-12-31 14:24:32
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
