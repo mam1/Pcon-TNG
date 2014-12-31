7c2698eb930c4221e42d0c9719f352fc431726ba
mam1 <mam1@cornell.edu>
2014-12-31 16:47:09 -0500

    make is working on the bone

--- Pcon-BBB/sd_card/sd_card.c
+++ Pcon-BBB/sd_card/sd_card.c
@@ -2,12 +2,12 @@
 * @Author: mam1
 * @Date:   2014-12-31 14:13:08
 * @Last Modified by:   mam1
-* @Last Modified time: 2014-12-31 14:24:14
+* @Last Modified time: 2014-12-31 16:37:24
 */
 
 #include <stdio.h>
 
-int main() {
+int sd_open() {
 
     return 0;
 }
--- Pcon-BBB/sd_card/test_sd_card.c
+++ Pcon-BBB/sd_card/test_sd_card.c
@@ -2,12 +2,12 @@
 * @Author: mam1
 * @Date:   2014-12-31 14:15:27
 * @Last Modified by:   mam1
-* @Last Modified time: 2014-12-31 14:24:32
+* @Last Modified time: 2014-12-31 16:45:31
 */
 
 #include <stdio.h>
 
 int main() {
-
+	printf("\n\n***************************************\n");
     return 0;
 }
