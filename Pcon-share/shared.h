fb7aa815cd130db507734915c5b350b0c83b8438
Mark Mara <markmara@cornell.edu>
2015-02-10 22:12:06 -0500

    template maint seems to be working, but it does not handle input error well

--- Pcon-share/shared.h
+++ Pcon-share/shared.h
@@ -4,7 +4,7 @@
 /* version info */
 #define _major_version 			2
 #define _minor_version 			2
-#define _minor_revision 		2
+#define _minor_revision 		3
 
 /* shared bone-C3 commands */
 #define _PING         	27
