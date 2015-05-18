ad2c3b319b0be5e2d326e78af1354f1746840488
mam1 <mam1@cornell.edu>
2015-05-18 12:54:45 -0400

    replace with code from comtest2

--- Pcon-C3/Pcon.c
+++ Pcon-C3/Pcon.c
@@ -1,198 +1,92 @@
-/*
- * Pcon.c
- *
- *  Created on: Nov 28, 2014
- *      Author: mam1
+/**
+ * This is the main comtest2 program file.
  */
 
 
-#include <stdio.h>
-#include <unistd.h>		//sleep
-#include <stdint.h>		//uint_8, uint_16, uint_32, etc.
-#include "propeller.h"
-#include "Pcon.h"
-#include "../Pcon-share/schedule.h"
-// #include "test.h"
-#include "simpletools.h"                      // Include simple tools
+//#include <string.h>
 #include "simpletext.h"
 #include "fdserial.h"
+#include "packet.h"
+#include <stdlib.h>
+//#include <strings.h>
+#include <unistd.h>
 
-#define RX                      1
-#define TX                      0
-#define MODE                    0
-#define BAUD                 9600
-#define SERIAL_BUFFER_SIZE    128
-#define START_COMMAND         27
+fdserial          *pktport;
+_packet           g_packet,r_packet;
+_schedule_frame   s_frame, s_frame_read; 
 
-/***************** global code to text conversion ********************/
-const char *day_names_long[7] = {
-     "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-const char *day_names_short[7] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
-const char *onoff[2] = {"off"," on"};
-const char *con_mode[3] = {"manual","  time","time & sensor"};
-const char *sch_mode[2] = {"day","week"};
-const char *c_mode[4] = {"manual","  time","   t&s"," cycle"};
 
-// /*************************** drivers  ********************************/
-// extern _Driver _FullDuplexSerialDriver;
-// extern _Driver _FileDriver;
-// _Driver *_driverlist[] = {&_FullDuplexSerialDriver,&_FileDriver,NULL}; 
+uint32_t          sch[_SCHEDULE_SIZE];
+  
 
-/***************************** externals ******************************/
+int main(void)
+{
+  uint8_t         *byte_ptr;
+  int               i;
+  
+  printi("\n***********************\ncomtest 2.0\n***********************\n\n");
+  pktport = fdserial_open(PROD_RX, PROD_TX, PROD_MODE, PROD_BAUD);
+  if(pktport == 0){
+    printi("*** packet port open error\n");
+    return 1;
+  }    
+  
+  sch[0] = 3;
+  sch[1] = 1000877;
+  sch[2] = 128274849;
+  sch[3] = 183838533;
+  
+  printi("packet port opened\n");
+  printi("starting packet processing cog ..... ");
+  packet_start(pktport);
+//  printi("g_packet size = %d\n",sizeof(g_packet));
+//  printi("s_frame size = %d\n",sizeof(s_frame));
+//  printi("size of header = %d\n", 4);
+//  printi("size of schedule = %d\n",4+(sch[0]*4));
+//  printi("number of schedule records = %d\n",sch[0]);
+//  printi("packet length = %d\n",sch[0]*4+8);
+  
+  if(make_schedule_frame(&g_packet,(uint8_t *)&s_frame,sizeof(s_frame),6,7,sch)){
+    printi("*** make_schedule_frame error\n");
+    return 1;
+  }
+  packet_send(pktport,&g_packet);
 
-/* get a block of data from the bone */
-void get_block(fdserial *fd,uint8_t *buf_ptr,int count){ 
-   while(count-- > 0){ 
-        while (fdserial_rxReady(fd) == 0);      //wait for something to show up in the buffer
-        *buf_ptr++ = fdserial_rxChar(fd);       //grab a byte
-    }
-    return;
+  sch[0] = 3;
+  sch[1] = 99999999;
+  sch[2] = 88888888;
+  sch[3] = 77777777; 
+    
+  if(make_schedule_frame(&g_packet,(uint8_t *)&s_frame,sizeof(s_frame),6,7,sch)){
+    printi("*** make_schedule_frame error\n");
+    return 1;
+  }
+  packet_send(pktport,&g_packet);
+  printi("loop waiting for a packet to appear on queue\n");    
+  printi("******************************************************\n");
+  for(;;){
+      printi("checking for packets\n");
+      if(packet_ready()){                         // see if there is a packet ready
+        printi(">>>> packet ready\n");
+        packet_read(&r_packet);                   // dequeue a packet
+        byte_ptr = (uint8_t *)&r_packet;
+        byte_ptr++;                               // set pointer to start of packet data
+        switch(*byte_ptr)                         // unpack frame based on frame type
+        {
+          case _SCHEDULE_P:                       // schedule frame
+            printi("recieved a schedule frame\n");
+            unpack_schedule_frame(byte_ptr,&s_frame_read);
+            schedule_frame_print(&s_frame_read);
+            break;
+          default:
+            printi("*** unknown packet type <%2x>\n",*byte_ptr); 
+        }                  
+//        packet_print(&r_packet); 
+      }
+      sleep(1);
+//      packet_send(pktport,&g_packet);
+  }      
+   
+  return 0;
 }
-
-int main(int argc, char *argv[]){
-    fdserial            *C3port;
-    int                 C3byte;
-    int                 out_byte;
-    int                 *size;
-    uint8_t             s[4], *load_byte;
-    uint32_t            sch[_DAYS_PER_WEEK][_NUMBER_OF_CHANNELS][_SCHEDULE_SIZE], *sch_ptr;
-    SYS_DAT             sys_dat;
-
-    int             i, get_bytes;
-    int             c;
-    uint8_t             *buf_ptr;
-    int                 int_buf;
-
-    sleep(1);
-    printf("\033\143"); //clear the terminal screen, preserve the scroll back
-    disp_sys();
-
-    sch_ptr = (uint32_t *)sch;
-
-    printf("schedule size %i bytes\r\n",sizeof(sch));
-    size = (int *)&s[0];
-    printf("open serial port\n");
-    C3port = fdserial_open(RX, TX, MODE, BAUD); //open io port to C3
-    printf("C3port = %i\n",C3port);
-    fdserial_rxFlush(C3port);           // flush input buffer
-    fdserial_txFlush(C3port);           // flush transmit buffer
-
-    while(1){
-        printf("wait for anything from the bone\n");
-        while (fdserial_rxReady(C3port) == 0);      //wait for something to show up in the buffer
-        C3byte = fdserial_rxChar(C3port);           //grab a byte
-        printf("got a <%u> from the bone\n",C3byte);
-        switch(C3byte){
-            case _PING:
-                printf("received a _PING, sending ACK <%u>\n\n",out_byte);
-                out_byte = _ACK;
-                fdserial_txChar(C3port, out_byte); 
-                break;
-            case _RECEIVE_SYS_DAT:
-                get_bytes = sizeof(sys_dat);
-                load_byte = (uint8_t *)&sys_dat;
-                printf("received a _RECIEVE_SYS_DAT, reading %i bytes from the bone\r\n",get_bytes);
-                out_byte = _ACK;
-                fdserial_txChar(C3port, out_byte);
-
-                while(get_bytes-- > 0){
-                    out_byte = _ACK;
-                    fdserial_txChar(C3port, out_byte);
-                    while (fdserial_rxReady(C3port) == 0);                            
-                    *load_byte = fdserial_rxChar(C3port);         //grab a byte
-                    load_byte++;                    
-                }
-                printf("system data %i.%i.%i loaded\n",sys_dat.major_version, sys_dat.minor_version, sys_dat.minor_revision);
-                break;
-            case _WRITE_SCH:
-                get_bytes = sizeof(sch);
-                printf("received a _WRITE_SCH, reading %i bytes from the bone\r\n",get_bytes);
-
-                out_byte = _ACK;
-                fdserial_txChar(C3port, out_byte);
-
-                while(get_bytes-- > 0){
-                    out_byte = _ACK;
-                    fdserial_txChar(C3port, out_byte);
-                    while (fdserial_rxReady(C3port) == 0){
-                        // pause(20);
-                    };                            
-                    *sch_ptr = fdserial_rxChar(C3port);         //grab a byte
-
-                    // printf("<%u> recieved\r\n", *sch_ptr);
-                    sch_ptr++;                    
-                    // printf("\n\rbyte written\n\rget_bytes = %i\r\n",get_bytes);
-
-                }
-                printf("schedule received from the bone\n\r");
-                // disp_all_schedules(CMD_FSM_CB *cb,uint32_t *sch)
-                break;
-            case _INT:
-                get_bytes = 4;
-                buf_ptr = (uint8_t *)&int_buf;
-                printf("received a _INT, reading %i bytes from the bone\r\n",get_bytes);
-                while(get_bytes-- > 0){
-                    *buf_ptr++ = fdserial_rxTime(C3port, 200);  //grab a byte
-                    if(*buf_ptr == -1){
-                        printf("*** read time out in receiving int\n");
-                        *buf_ptr = '\0';
-                    }                           
-                }
-                printf("received %i\n",int_buf);
-                break;
-            case _SYSTEM:
-                printf("received a request to reload local copy of system data from the bone\n");
-                int_buf = get_int(C3port);
-                printf("bone sending %i bytes\n",int_buf);
-                get_block(C3port,(uint8_t *)&sys_dat, int_buf);
-                printf("system data %i.%i.%i loaded\n",sys_dat.major_version, sys_dat.minor_version, sys_dat.minor_revision);
-
-                break;
-            case _PUSH_STATS:
-                break;
-            default:
-                printf("unknown command received from the bone <%u>\n",C3byte);
-        }
-    }
-
-
-    printf("\nnormal termination\n");
-    return 0;
-}
-
-void disp_sys(void) {
-	printf("\n*** Pcon  %d.%d.%d ***\n\n", _major_version, _minor_version, _minor_revision);
-	return;
-}
-
-
-/* get an int from the bone */
-int get_int(fdserial *fd){
-    int         get_bytes = 4;
-    int         int_buf = 0;
-    uint8_t     *buf_ptr;
-
-    buf_ptr = (uint8_t *)&int_buf;
-    // printf("receiving a _INT, reading %i bytes from the bone\r\n",get_bytes);
-    while(get_bytes-- > 0){
-        *buf_ptr = fdserial_rxTime(fd, 200);  //grab a byte
-        if(*buf_ptr == -1){
-            printf("*** read time out in get_int\n");
-            *buf_ptr = '\0';
-        }
-        // printf("received <%u>",*buf_ptr);
-        buf_ptr++;                           
-    }
-    // printf("received %i\n",int_buf);    
-    return int_buf;
-}
-
-
-
-// void term(FdSerial_t *fd){
-//     s_close(fd);
-//     printf("serial port closed \n");
-//     return;
-// }
-
 
---
+++ Pcon-C3/packet.h
@@ -0,0 +1,77 @@
+/**
+ * @file packet.h
+ */
+
+#ifndef __PACKET_H__
+#define __PACKET_H__
+
+
+/* Ascii control Characters */
+#define      _SOH  0x01
+#define      _STX  0x02
+#define      etx  0x03
+#define      eot  0x04
+#define      ack  0x06
+#define      nak  0x15
+#define      syn  0x16
+
+/* packet types */
+#define _SCHEDULE_P   10 //schedule packet
+
+#define CONS_RX 0
+#define CONS_TX 1
+#define CONS_MODE 0
+#define CONS_BAUD 9600   //115200
+
+#define PROD_RX 1
+#define PROD_TX 0
+#define PROD_MODE 0
+#define PROD_BAUD CONS_BAUD
+
+#define PACKET_DLEN 128
+#define PACKET_QLEN 16 // power of 2 for queue
+#define PACKET_QMASK (PACKET_QLEN-1)
+
+#include <stdint.h>
+#include "simpletext.h"
+#include "fdserial.h"
+#include "packet.h"
+#include "shared.h"
+
+
+typedef struct _packetruct {
+    uint8_t length; // total packet length including length and checksum byte
+    uint8_t data[PACKET_DLEN+1]; // add 1 for checksum
+} _packet;
+
+typedef struct {
+  uint8_t       f_type;
+  uint8_t       day;
+  uint8_t       channel;
+  uint8_t       rcnt;
+  uint32_t      rec[_MAX_SCHEDULE_RECS];
+} _schedule_frame;
+
+typedef union {uint32_t MyLong; uint8_t MyByte[4]; } _packed;
+    
+//void _packetart(fdserial *rec);
+//void _packetop(void);
+
+void packet_start(fdserial *rec);
+void packet_stop(void);
+    
+int packet_make(_packet *pkt, uint8_t *data, int len);
+int make_schedule_frame(_packet *pkt,uint8_t *data,int len,int day,int channel,uint32_t *sch);
+int packet_send(text_t *port, _packet *pkt);
+int packet_ready(void);
+int packet_read(_packet *pkt);
+int packet_print(_packet *pkt);
+int schedule_frame_print(_schedule_frame *f);
+
+void PackLong(uint8_t *p, _packed N);
+uint32_t UnPackLong(uint8_t *p);
+int pack_schedule_frame(_schedule_frame *);
+int unpack_schedule_frame(uint8_t *byte_ptr, _schedule_frame *sf_ptr);
+  
+  
+#endif
