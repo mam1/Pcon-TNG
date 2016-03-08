22c2a62fc32f2d971f89b85163683ce9a76a8ae3
mam1 <mam1@cornell.edu>
2016-03-08 10:22:58 -0500

    test-cgi first cut working

---
+++ BBB/test-cgi/test-cgi.c
@@ -0,0 +1,26 @@
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>		//sleep
+#include <stdint.h>		//uint_8, uint_16, uint_32, etc.
+#include <ctype.h> 		//isalnum, tolower
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include "shared.h"
+#include "ipc.h"
+#include "Pcon.h"
+#include "shared.h"
+#include "bitlit.h"
+#include "PCF8563.h"
+#include "schedule.h"
+#include "BBBiolib.h"
+#include "trace.h"
+
+int main(void) {
+    printf("Content-type: text/html\n\n");
+    printf("Hi from the CGI!\n");
+    return 0;
+}
