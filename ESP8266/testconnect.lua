ac461394c5b59281d8cd10e91fea40b282ea2323
mam1 <mam1@cornell.edu>
2016-03-22 15:40:56 -0400

    add code to read DHT22 sensor, working

--- ESP8266/testconnect.lua
+++ ESP8266/testconnect.lua
@@ -1,29 +1,35 @@
+ --get data from DHT22 sensor on <pin>
+function rdDHT22(pin)
+   local tmp, hmd
+    print("\n  reading pin ".. pin)
+    dht22 = require("dht22_min")
+    --read sensor
+    dht22.read(pin)
+    tmp = dht22.getTemperature()
+    hmd = dht22.getHumidity()
+    if tmp == nil then
+        print("*** Error reading temperature from DHT22")
+    end
+    if hmd == nil then
+        print("*** Error reading humidity from DHT22")
+    end
+    --release DHT22 module
+    dht22 = nil
+    package.loaded["dht22"]=nil
+    tmp = (tmp*9)/5 + 320
+    return tmp, hmd 
+end
+ 
  tmr.alarm(0,30000, 1, function()
-
  snum = 2
- temp = 780
- humidity = 450
+ temp,humidity = rdDHT22(7)
+ --temp = 55
+ --humidity = 15
  conn=net.createConnection(net.TCP, 0)
  conn:on("receive", function(conn, payload) print(payload) end )
  conn:connect(8080,"192.168.254.34")
- 
  mess = "GET /cgi-bin/Scon?snesor=" .. snum .. "&temp=" .. temp .. "&humid=" .. humidity .. " HTTP/1.1\r\nHost: 192.168.254.34\r\n"
  .."Connection: keep-alive\r\nAccept: */*\r\nUser-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n\r\n"
  conn:send(mess)
  
- --conn:send("GET /cgi-bin/Scon?snesor=" .. snum .. "&temp=" .. t .. " HTTP/1.1\r\nHost: 192.168.254.34\r\n"
--- .."Connection: keep-alive\r\nAccept: */*\r\nUser-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n\r\n")
-
- -- conn:send("GET /update?api_key="
- --        .. key
- --        .. "&field"
- --        .. field
- --        .."=" 
- --        .. value
- --        .. " HTTP/1.1\r\n"
- --        .. "Host: 192.168.254.34\r\n"
- --        .. "Connection: close\r\n"
- --        .. "Accept: */*\r\n"
- --        .. "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n"
- --        .. "\r\n")
  end)
