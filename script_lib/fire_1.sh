4f220445c21d5e39b6331873dfac25d58dfe9f4c
mam1 <mam1@cornell.edu>
2016-05-24 10:30:40 -0400

    adding statup script

---
+++ script_lib/fire_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# start up script for 16 channel controler
+
+#check out what is running
+
+echo " 
+*************************
+  starting setup script
+
+  checking for active processes
+"
