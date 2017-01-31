cdc2d098f33f2ce54e9fbeba5f6e706f61d9c338
mam1 <mam1@cornell.edu>
2017-01-31 15:41:12 -0500

    new sensor log processing code running on Mint box

--- Mint/process-master/process.c
+++ Mint/process-master/process.c
@@ -11,7 +11,7 @@
 // #include "ipc.h"
 #include "Pcon.h"
 // #include "bitlit.h"
-#include "PCF8563.h"
+// #include "PCF8563.h"
 // #include "gpio.h"
 // #include "led.h"
 // #include "schedule.h"
@@ -37,20 +37,49 @@ int main (void) {
 		_tm 		ts;
 	} buffer;
 
-	sensor_data = fopen(_SENSOR_LOG_FILE_NAME,"r");
+	int 			rcnt;
+	int 			snum;
+	int 			avg[_NUMBER_OF_SENSORS];
+	int 			sum[_NUMBER_OF_SENSORS];
+	int 			sfound[_NUMBER_OF_SENSORS];
+	int 			i;
+
+
+
+	printf("\nprocess sensor data v 0.0.3\r\n");
+	printf("opening %s\n",_SENSOR_MASTER_FILE_NAME);
+
+	sensor_data = fopen(_SENSOR_MASTER_FILE_NAME,"r");
 	if(sensor_data == NULL){
 		printf("  Error: %d (%s)\n", errno, strerror(errno));
-		printf("    attempting to open %s\n\n application terminated\n\n", _SENSOR_LOG_FILE_NAME);
+		printf("    attempting to open %s\n\n application terminated\n\n", _SENSOR_MASTER_FILE_NAME);
 		return 1;
 	}
-	printf("  %s opened\n",_SENSOR_LOG_FILE_NAME);
+	rcnt = 0;
+
 	while(fread(&buffer, sizeof(buffer), 1, sensor_data) == 1){
-		printf("  %02i:%02i:%02i  %s %02i/%02i/%02i sensor %i temp %i humidity %i\n",
-	       buffer.ts.tm_hour, buffer.ts.tm_min, buffer.ts.tm_sec, day_names_long[buffer.ts.tm_wday], 
-	       buffer.ts.tm_mon, buffer.ts.tm_mday, buffer.ts.tm_year, buffer.sensor_id, buffer.temp, buffer.humidity);
+		rcnt++;
+		if(buffer.sensor_id < 0 || buffer.sensor_id > (_NUMBER_OF_SENSORS - 1))
+			printf("  sensor id out of range  <%i>\n", buffer.sensor_id);
+		else
+			sfound[buffer.sensor_id] = 1;
+
+		sum[buffer.sensor_id] += buffer.temp;
+		// printf("  %02i:%02i:%02i  %s %02i/%02i/%02i sensor %i temp %i humidity %i\n",
+	 //       buffer.ts.tm_hour, buffer.ts.tm_min, buffer.ts.tm_sec, day_names_long[buffer.ts.tm_wday], 
+	 //       buffer.ts.tm_mon, buffer.ts.tm_mday, buffer.ts.tm_year, buffer.sensor_id, buffer.temp, buffer.humidity);
 	}
 
 	fclose(sensor_data);
+
+	for(i=0; i<_NUMBER_OF_SENSORS; i++)
+		avg[i] = sum[i] / rcnt;
+
+	printf("closing %s\n",_SENSOR_MASTER_FILE_NAME);
+	printf("%i records processed\n\r", rcnt);
+	printf("    average sensor value\n");
+	for(i=0; i<_NUMBER_OF_SENSORS; i++)
+		printf("      sensor %i <%i>\n", i, avg[i]);
 
   return (0);
 }
