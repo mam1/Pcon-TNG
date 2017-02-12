cc8e120f29e19007af9524ff1ce24510ccb76059
mam1 <mam1@cornell.edu>
2017-02-12 11:17:25 -0500

    add record cont display

--- Mint/process-master/process.c
+++ Mint/process-master/process.c
@@ -112,8 +112,8 @@ int main (void) {
 	{
 		rcnt++;
 		printf("\n");
-		printf("    %06i records read\r\n", rcnt);
-		sleep(100);
+		printf("    %06i records read\r", rcnt);
+		sleep(1);
 		if(buffer.sensor_id < 0 || buffer.sensor_id > _MAX_SENSOR_ID)
 		{
 			printf("  sensor id out of range  <%i>\n", buffer.sensor_id);
