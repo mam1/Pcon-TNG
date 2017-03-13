f6e4d985a852c725974ead7b1fc94e2fc9cad715
mam1 <mam1@cornell.edu>
2017-03-13 14:10:33 -0400

    confused

---
+++ BBB/tests/test-date/test_date.c
@@ -0,0 +1,23 @@
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>		//sleep
+#include <stdint.h>		//uint_8, uint_16, uint_32, etc.
+#include <ctype.h> 		//isalnum, tolower
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h>
+
+
+
+int main(void){
+
+	time_t			t;
+
+	printf("local: %s", asctime(localtime(&t)));
+
+	return 0;
+}
