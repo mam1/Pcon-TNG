693d0be1e53372c62b99f968e8b6a09c39526e7d
mam1 <mam1@cornell.edu>
2017-05-01 11:42:21 -0400

    change for new directory layout on ShopMint

--- script_lib/Mint/bkup_BH.sh
+++ script_lib/Mint/bkup_BH.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo -n $(date) >> /media/logs/bkup-log
-echo backup started >> /media/logs/bkup-log
+export PATH=$PATH:/usr/local/myscripts
 
-mount -lv /dev/sdc1  /mnt/BHbackup
-rsync   -avH     /media/BlackHole/ /mnt/BHbackup/ >> /media/logs/bkup-log
-umount /mnt/BHbackup
+echo -n $(date) >> /media/logs/bkup-log.log
+echo ' backup started' >> /media/logs/bkup-log.log
 
-echo -n $(date) >> /media/logs/bkup-log
-echo backup finished >> /media/logs/bkup-log
+mount -lv /dev/sdc1  /mnt/BlackHole_bkup
+rsync   -avH     /media/BlackHole/ /mnt/BlackHole_bkup/ >> /media/logs/bkup-log.log
+umount /mnt/BlackHole_bkup
+
+echo -n $(date) >> /media/logs/bkup-log.log
+echo ' backup finished' >> /media/logs/bkup-log.log
+echo '**********************************************' >> /media/logs/bkup-log.log
