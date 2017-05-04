5ce340641066814701f5896f8e8ae0cf28790451
mam1 <mam1@cornell.edu>
2017-05-04 16:13:34 -0400

    cleaning up daemon

--- BBB/daemon/Dcon.c
+++ BBB/daemon/Dcon.c
@@ -210,14 +210,14 @@ int main(void) {
 	   we can exit the parent process. */
 	if (pid > 0) {
 		/* create pid file */
-		fopen($PID_FILE_NAME, "w");
+		fopen(_PID_FILE_NAME, "w");
 		if (pidf != NULL){
 			
 			fprintf(pidf,"%i", pid);
 			fclose(pidf);
 		}
 		else{
-			printf(" can't write pid file <%s>\n", PID_FILE_NAME);
+			printf(" can't write pid file <%s>\n", _PID_FILE_NAME);
 			exit(EXIT_FAILURE);
 		}
 		exit(EXIT_SUCCESS);
