6965b54fde576cb044052473c3da384b31f425d8
mam1 <mam1@cornell.edu>
2017-05-08 10:23:23 -0400

    new io lib working

--- BBB/tests/test-gpio/gpio.c
+++ BBB/tests/test-gpio/gpio.c
@@ -30,47 +30,47 @@
 #include "bbb.h"
 
 /* gpio enable list */
-#define _NUMBER_OF_GPIOS	17
-#define _GPIOS              38,39,34,35,66,67,69,68,45,44,23,26,47,46,27,65,22	
-#define _HEADERS			 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8
-#define _PINS  				 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19 		
+// #define _NUMBER_OF_GPIOS	17
+// #define _GPIOS              38,39,34,35,66,67,69,68,45,44,23,26,47,46,27,65,22	
+// #define _HEADERS			 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8
+// #define _PINS  				 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19 		
 
-/* map P8 header pins */
-#define _LED_1			7	
-#define _LED_2			9		
-#define _LED_3			8			
-#define _LED_4			10
-#define _DIOB_DIN 		11
-#define _DIOB_DATA_RLY	11
-#define _DIOB_SCLK_IN	12
-#define	_DIOB_SCLK_RLY	12
-#define	_DIOB_LAT_RLY	14
-#define	_DIOB_LOAD_IN	27
-#define _R1_CAPE		16
-#define _R2_CAPE		17
-#define _R3_CAPE		18
-#define _R4_CAPE		26
+// /* map P8 header pins */
+// #define _LED_1			7	
+// #define _LED_2			9		
+// #define _LED_3			8			
+// #define _LED_4			10
+// #define _DIOB_DIN 		11
+// #define _DIOB_DATA_RLY	11
+// #define _DIOB_SCLK_IN	12
+// #define	_DIOB_SCLK_RLY	12
+// #define	_DIOB_LAT_RLY	14
+// #define	_DIOB_LOAD_IN	27
+// #define _R1_CAPE		16
+// #define _R2_CAPE		17
+// #define _R3_CAPE		18
+// #define _R4_CAPE		26
  
-/* map P9 header pins  */
-#define _R5_CAPE		12
-#define _R6_CAPE		15
-#define _R7_CAPE		23
-#define _R8_CAPE		41
+// /* map P9 header pins  */
+// #define _R5_CAPE		12
+// #define _R6_CAPE		15
+// #define _R7_CAPE		23
+// #define _R8_CAPE		41
 
 /* initialise gpio pins */
-int init_gpio(void) {
-	char 				command[120];
-	int 				i;
-	int 				gpios[_NUMBER_OF_GPIOS] = {_GPIOS};
+// int init_gpio(void) {
+// 	char 				command[120];
+// 	int 				i;
+// 	int 				gpios[_NUMBER_OF_GPIOS] = {_GPIOS};
 
-	for (i = 0; i < _NUMBER_OF_GPIOS; i++) {
-		sprintf(command, "if [ ! -d /sys/class/gpio/gpio%i ]; then echo %i > /sys/class/gpio/export; fi", gpios[i], gpios[i] );
-		// logit(command);
-		printf("system command %s returned %i\n", command, system(command));
-	}
+// 	for (i = 0; i < _NUMBER_OF_GPIOS; i++) {
+// 		sprintf(command, "if [ ! -d /sys/class/gpio/gpio%i ]; then echo %i > /sys/class/gpio/export; fi", gpios[i], gpios[i] );
+// 		// logit(command);
+// 		printf("system command %s returned %i\n", command, system(command));
+// 	}
 
-	return 0;
-}
+// 	return 0;
+// }
 
 /* get a line of input from the console */
 // char *getline(void) {
@@ -107,7 +107,7 @@ int init_gpio(void) {
 
 int main(void){
 
-	int 		rnum,state;
+	int 		rnum,state,i;
 
 
 	// switch (*getline()){
@@ -183,10 +183,11 @@ int main(void){
 			printf("enter 0 for off or 1 for on > ");
 			scanf ("%d",&state);
 			if ((state == 0) || (state == 1))
-				printf("rnum %i set to %i\n", rnum, state);
-				digital_output(P9, 12, 1);
-			    sleep(1);
-			    digital_output(P9, 12, 0);
+				printf("rnum %i set to %i\n\n", rnum, state);
+				if(state == 1)
+					digital_output(P9, 12, 1);
+				else
+				    digital_output(P9, 12, 0);
 		}
 
 	}
