1e637e3abf6859cf9bff86eea281cc52a48d9987
mam1 <mam1@cornell.edu>
2017-05-11 16:59:18 -0400

    trying to accesss HDMI pins

--- BBB/include/shared.h
+++ BBB/include/shared.h
@@ -17,13 +17,13 @@
 #define _FILE_NAME_SIZE 		50
 // #define _CGI_DATA_FILE_PREFIX	"/media/TOSHIBA/sensor"		// cgi generated log of sensor data
 // #define _CGI_DATA_FILE_SUFIX	".dat"							// cgi generated log of sensor data
-#define _SENSOR_DATA_FILE_NAME		"/media/sensor.dat" 		// USB drive collecting sensor readingss
-#define _CGI_LOG_FILE_NAME			"/media/CGI.log"			// log file name
-// #define _SYSTEM_FILE_NAME			"/home/Pcon-data/sys.dat"				// system data file
+#define _SENSOR_DATA_FILE_NAME		"/media/data/sensors/sensor.dat" 		// USB drive collecting sensor readingss
+#define _CGI_LOG_FILE_NAME			"/media/data/logs/CGI.log"				// log file name
+// #define _SYSTEM_FILE_NAME			"/home/Pcon-data/sys.dat"			// system data file
 #define _IPC_FILE_NAME				"/home/Pcon-data/ipc.dat"   			// memory mapped ipc file
 #define _IPC_FILE_BACKUP_NAME		"/home/Pcon-data/ipc.bkup"   			// ipc backup file 
-#define _TRACE_FILE_NAME			"/home/Pcon-data/trace.dat" 			// trace file
-#define _DAEMON_LOG 				"/home/Pcon-data/daemon.log" 			// daemon log file name
+#define _TRACE_FILE_NAME			"/hmedia/data/trace/Pcon-trace.dat" 	// trace file
+#define _DAEMON_LOG 				"/media/data/logs/daemon.log" 			// daemon log file name
 #define _SENSOR_MASTER_FILE_NAME    "/media/Raid/sensor logs/master.dat"	// cumulative history of sensor readings
 #define _PID_FILE_NAME 				"/var/run/Dcon.pid"
 
--- BBB/tests/test-gpio/gpio.c
+++ BBB/tests/test-gpio/gpio.c
@@ -4,11 +4,6 @@
 
 */
 
-// #include <stdio.h>
-// #include <stdint.h>		//uint_8, uint_16, uint_32, etc.
-// #include <sys/types.h>
-// #include <sys/stat.h> 
-
 #include <sys/sem.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
@@ -26,150 +21,23 @@
 #include <fcntl.h>
 #include <time.h>
 
-// #include "iolib.h"
 #include "bbb.h"
 
-/* gpio enable list */
-// #define _NUMBER_OF_GPIOS	17
-// #define _GPIOS              38,39,34,35,66,67,69,68,45,44,23,26,47,46,27,65,22	
-// #define _HEADERS			 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8
-// #define _PINS  				 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19 		
-
-// /* map P8 header pins */
-// #define _LED_1			7	
-// #define _LED_2			9		
-// #define _LED_3			8			
-// #define _LED_4			10
-// #define _DIOB_DIN 		11
-// #define _DIOB_DATA_RLY	11
-// #define _DIOB_SCLK_IN	12
-// #define	_DIOB_SCLK_RLY	12
-// #define	_DIOB_LAT_RLY	14
-// #define	_DIOB_LOAD_IN	27
-// #define _R1_CAPE		16
-// #define _R2_CAPE		17
-// #define _R3_CAPE		18
-// #define _R4_CAPE		26
- 
-// /* map P9 header pins  */
-// #define _R5_CAPE		12
-// #define _R6_CAPE		15
-// #define _R7_CAPE		23
-// #define _R8_CAPE		41
-
-/* initialise gpio pins */
-// int init_gpio(void) {
-// 	char 				command[120];
-// 	int 				i;
-// 	int 				gpios[_NUMBER_OF_GPIOS] = {_GPIOS};
-
-// 	for (i = 0; i < _NUMBER_OF_GPIOS; i++) {
-// 		sprintf(command, "if [ ! -d /sys/class/gpio/gpio%i ]; then echo %i > /sys/class/gpio/export; fi", gpios[i], gpios[i] );
-// 		// logit(command);
-// 		printf("system command %s returned %i\n", command, system(command));
-// 	}
-
-// 	return 0;
-// }
-
-/* get a line of input from the console */
-// char *getline(void) {
-//     char 		*line = malloc(100), *linep = line;
-//     size_t 		lenmax = 100, len = lenmax;
-//     int 		c;
-
-//     if(line == NULL)
-//         return NULL;
-
-//     for(;;) {
-//         c = fgetc(stdin);
-//         if(c == EOF)
-//             break;
-
-//         if(--len == 0) {
-//             len = lenmax;
-//             char * linen = realloc(linep, lenmax *= 2);
-
-//             if(linen == NULL) {
-//                 free(linep);
-//                 return NULL;
-//             }
-//             line = linen + (line - linep);
-//             linep = linen;
-//         }
-
-//         if((*line++ = c) == '\n')
-//             break;
-//     }
-//     *line = '\0';
-//     return linep;
-// }
 
 int main(void){
 
-	int 		rnum,state,i;
+	int 		rnum,state,lcount,i,ii;
 
+	typedef struct {
+		int         header;
+		int         pin;
+	} _LED;
 
-	// switch (*getline()){
-	// 	case
+	_LED 			led[19] = {{9,12},{9,15},{9,23},{8,27}};
+	lcount = 4;
 
-
-
-	// }
-
-	/* initialise gpio access */
-	// iolib_init();
-
-
-
-	// init_gpio();
-	// iolib_init();
-
-
-
-	// /* setup gpio access to LEDs on the WaveShare Cape*/
-	// logit("iolib initialized");
-	// logit("mapping WaveShare Misc Cape leds to gpio pins");
-	// iolib_setdir(8, _LED_1, BBBIO_DIR_OUT);
-	// iolib_setdir(8, _LED_2, BBBIO_DIR_OUT);
-	// iolib_setdir(8, _LED_3, BBBIO_DIR_OUT);
-	// iolib_setdir(8, _LED_4, BBBIO_DIR_OUT);
-
-	// /* turn off the LEDs */
-	// pin_low(8, _LED_1);
-	// pin_low(8, _LED_2);
-	// pin_low(8, _LED_3);
-	// pin_low(8, _LED_4);
-
-	// /* setup gpio access to PhotoMos relays */
-	// logit("mapping PhotoMos relays to gpio pins");
-	// iolib_setdir(8, _R1_CAPE, BBBIO_DIR_OUT);
-	// iolib_setdir(8, _R2_CAPE, BBBIO_DIR_OUT);
-	// iolib_setdir(8, _R3_CAPE, BBBIO_DIR_OUT);
-	// iolib_setdir(8, _R4_CAPE, BBBIO_DIR_OUT);
-	// iolib_setdir(9, _R5_CAPE, BBBIO_DIR_OUT);
-	// iolib_setdir(9, _R6_CAPE, BBBIO_DIR_OUT);
-	// iolib_setdir(9, _R7_CAPE, BBBIO_DIR_OUT);
-	// iolib_setdir(9, _R8_CAPE, BBBIO_DIR_OUT);
-
-	// /* turn all relays off */
-	// pin_low(8, _R1_CAPE);
-	// pin_low(8, _R2_CAPE);
-	// pin_low(8, _R3_CAPE);
-	// pin_low(8, _R4_CAPE);
-	// pin_low(9, _R5_CAPE);
-	// pin_low(9, _R6_CAPE);
-	// pin_low(9, _R7_CAPE);
-	// pin_low(9, _R8_CAPE);
-
-	// /* setup gpio access to serial header on the DIOB */
-	// logit("mapping DIOB serial header to gpio pins");
-	// iolib_setdir(8, _DIOB_DIN, BBBIO_DIR_OUT);
-	// iolib_setdir(8, _DIOB_SCLK_IN, BBBIO_DIR_OUT);
-	// iolib_setdir(8, _DIOB_LAT_RLY, BBBIO_DIR_OUT);
-	// pin_low(8, _DIOB_SCLK_IN);
-	// pin_low(8, _DIOB_DIN);
-
+	show_gpio();
+									
 	printf("enter number > 17 to exit\n");
 	for(;;){
 		printf("enter relay number 0 - 17 > ");
@@ -182,14 +50,12 @@ int main(void){
 		while((state != 0) && (state != 1)){
 			printf("enter 0 for off or 1 for on > ");
 			scanf ("%d",&state);
-			if ((state == 0) || (state == 1))
-				printf("rnum %i set to %i\n\n", rnum, state);
-				if(state == 1)
-					digital_output(P9, 12, 1);
-				else
-				    digital_output(P9, 12, 0);
-		}
+			if ((state == 0) || (state == 1)){
+				printf("led %i on header %i, pin %i set to %i\n", rnum, led[rnum].header, led[rnum].pin, state );
+				digital_output(led[rnum].header, led[rnum].pin, state);
+			}
 
+		}
 	}
 
 	return 0;
