3a87a59ae7add3cb7f533b98d3a7611de6054ef4
mam1 <mam1@cornell.edu>
2017-05-14 16:33:38 -0400

    controling pind by calling shell command <config-pin>

--- BBB/tests/test-gpio/gpio.c
+++ BBB/tests/test-gpio/gpio.c
@@ -25,23 +25,31 @@
 
 
 /*	gpio pin assignment {header,pin}  	*/
-#define _RLY1			{9,12}
-#define _RLY2			{9,15}
-#define _RLY3			{9,23}
-#define _RLY4			{8,27}
+#define _HB 			{8,27}
+#define _CHAN1			{9,12}
+#define _CHAN2			{9,15}
+#define _CHAN3			{9,23}
+#define _CHAN4			{8,28}
+#define _CHAN5			{8,29}
 
+int dout(){
 
+	return 0;
+}
 
 int main(void){
 
 	int 		rnum,state;
+	char 		header[10];
+	char 		pin[10];
+	char 		cmd[30];
 
 	typedef struct {
 		int         header;
 		int         pin;
 	} _GPIO;
 
-	_GPIO 			gpio[19] = {_RLY1,_RLY2,_RLY3,_RLY4};
+	_GPIO 			gpio[19] = {_HB,_CHAN1,_CHAN2,_CHAN3,_CHAN4,_CHAN5};
 
 
 	show_gpio();
@@ -54,14 +62,50 @@ int main(void){
 			printf("\nnormal exit\n");
 			return 0;
 		}
+		sprintf( header, "%d", gpio[rnum].header );
+		sprintf( pin, "%d", gpio[rnum].pin );
+
+
 		state = -1;
 		while((state != 0) && (state != 1)){
 			printf("enter 0 for off or 1 for on > ");
 			scanf ("%d",&state);
 			if ((state == 0) || (state == 1)){
 				printf("led %i on header %i, pin %i set to %i\n", rnum, gpio[rnum].header, gpio[rnum].pin, state );
-				digital_output(gpio[rnum].header, gpio[rnum].pin, state);
+
+
+				char 	highlow[5];
+				char 	*prefix="config-pin P";
+
+				switch(state){
+					case 0:
+						strcpy(highlow," low");
+						break;
+					case 1:
+						strcpy(highlow," hi");
+						break;
+					default:
+						break;
+				}
+
+				strcpy(cmd,prefix);
+				strcat(cmd,header);
+				strcat(cmd,".");
+				strcat(cmd,pin);
+				strcat(cmd,highlow);
+				printf("\n\n%s\n\n",cmd);
+				// digital_output(gpio[rnum].header, gpio[rnum].pin, state);
+				system(cmd);
+				show_gpio();
 			}
+
+
+
+
+
+
+
+
 
 		}
 	}
