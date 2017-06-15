43cef2cd1add683ba23d4977699f0eb3caa68d87
mam1 <mam1@cornell.edu>
2017-06-15 10:46:38 -0400

    back up script working

--- script_lib/Mint/bkup_BH.sh
+++ script_lib/Mint/bkup_BH.sh
@@ -9,5 +9,12 @@ rsync   -avH  --delete  /media/BlackHole/ /mnt/BlackHole_bkup/ >> /media/logs/bk
 umount /mnt/BlackHole_bkup
 
 echo -n $(date) >> /media/logs/bkup-log.log
+echo 'Abyss backup started' >> /media/logs/bkup-log.log
+
+mount -lv /dev/sdc1  /mnt/Abyss_bkup
+rsync   -avH  --delete  /media/Abyss/ /mnt/Abyss_bkup/ >> /media/logs/bkup-log.log
+umount /mnt/Abyss_bkup
+
+echo -n $(date) >> /media/logs/bkup-log.log
 echo ' backup finished' >> /media/logs/bkup-log.log
 echo '**********************************************' >> /media/logs/bkup-log.log
