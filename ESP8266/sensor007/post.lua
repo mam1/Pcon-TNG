49ff92f7f394cce26211ab7774d2af625bd90fa6
mam1 <mam1@cornell.edu>
2017-12-02 15:37:18 -0500

    trying to get CGI working

--- ESP8266/sensor007/post.lua
+++ ESP8266/sensor007/post.lua
@@ -62,7 +62,7 @@ end
  temp,humidity = rdDHT22(7,flip)
  conn=net.createConnection(net.TCP, 0)
  conn:on("receive", function(conn, payload) print(payload) end )
- conn:connect(8080,"192.168.254.34")
+ conn:connect(8080,"192.168.254.205")
  mess = "GET /cgi-bin/Scon?snesor=" .. sensor .. "&temp=" .. temp .. "&humid=" .. humidity .. "\r\n"
  conn:send(mess)
  end)
