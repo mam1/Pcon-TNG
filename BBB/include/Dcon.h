e22132cecfbc831e3abb650b775c935464891d2c
mam1 <mam1@cornell.edu>
2019-11-05 20:50:51 -0500

    get daemon compile working

--- BBB/include/Dcon.h
+++ BBB/include/Dcon.h
@@ -2,9 +2,9 @@
 #define DCONH_
 
 /* Dcon version info */
-#define _MAJOR_VERSION_Dcon		4
-#define _MINOR_VERSION_Dcon 	1
-#define _MINOR_REVISION_Dcon	1
+#define _MAJOR_VERSION_Dcon		5
+#define _MINOR_VERSION_Dcon 	0
+#define _MINOR_REVISION_Dcon	0
 
 #include "shared.h"
 
