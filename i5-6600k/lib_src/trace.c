1a0cf39eb6e09b96282021110db8a73e44d0fcae
mam1 <mam1@cornell.edu>
2020-02-25 16:20:40 -0500

    initial setup

---
+++ i5-6600k/lib_src/trace.c
@@ -0,0 +1,23 @@
+/*
+ * trace.c
+ *
+ *  Created on: Nov 1, 2014
+ *      Author: mam1
+ */
+
+/* trace version info */
+#define _MAJOR_VERSION    0
+#define _MINOR_VERSION    0
+#define _MINOR_REVISION   0
+
+#include <stdio.h>
+
+
+
+void trace3(char *name, char *caller, char *message, int number){
+	FILE *tracef;
+	tracef = fopen(name, "a");
+	fprintf(tracef,"%s: %s %i\n",caller,message,number);
+	fclose(tracef);
+	return;
+}
