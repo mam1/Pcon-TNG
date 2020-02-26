a41ed94b6694b8137e429b30a4482ef9e7be2c04
mam1 <mam1@cornell.edu>
2020-02-26 15:24:37 -0500

    getting closer

--- i5-6600k/hellow.c
+++ i5-6600k/hellow.c
@@ -1,93 +1,87 @@
-// hello.c
+
+
 #include <stdio.h>
- 
-int main() {
-    printf("Hello, world!\n");
-    return 0;
+#include <signal.h>
+#include <memory.h>
+#include <sys/time.h>
+
+#include "MQTTClient.h"
+
+volatile int toStop = 0;
+
+void usage(char** argv)
+{
+    printf("Usage: %s topic channel_token\n", argv[0]);
+    exit(-1);
 }
 
-// #include <stdio.h>
-// #include "MQTTClient.h"
-// #include <stdio.h>
-// #include <signal.h>
-// #include <memory.h>
-// #include <sys/time.h>
+void cfinish(int sig)
+{
+    signal(SIGINT, NULL);
+    toStop = 1;
+}
 
-// volatile int toStop = 0;
+void messageArrived(MessageData* md)
+{
+    MQTTMessage* message = md->message;
 
-// void usage(char** argv)
-// {
-//     printf("Usage: %s topic channel_token\n", argv[0]);
-//     exit(-1);
-// }
-
-// void cfinish(int sig)
-// {
-//     signal(SIGINT, NULL);
-//     toStop = 1;
-// }
-
-// void messageArrived(MessageData* md)
-// {
-//     MQTTMessage* message = md->message;
-
-//     printf("%.*s\t", md->topicName->lenstring.len, md->topicName->lenstring.data);
-//     printf("%.*s\n", (int)message->payloadlen, (char*)message->payload);
-// }
+    printf("%.*s\t", md->topicName->lenstring.len, md->topicName->lenstring.data);
+    printf("%.*s\n", (int)message->payloadlen, (char*)message->payload);
+}
 
 
-// int main(int argc, char** argv)
-// {
-//     int rc = 0;
-//     unsigned char buf[100];
-//     unsigned char readbuf[100];
+int main(int argc, char** argv)
+{
+    int rc = 0;
+    unsigned char buf[100];
+    unsigned char readbuf[100];
 
-//     if (argc < 3)
-//         usage(argv);
+    if (argc < 3)
+        usage(argv);
 
-//     char* host = "mqtt.beebotte.com";
-//     int port = 1883;
-//     char* topic = argv[1];
-//     char* clientid = "myid";
-//     char* username = argv[2];
+    char* host = "mqtt.beebotte.com";
+    int port = 1883;
+    char* topic = argv[1];
+    char* clientid = "myid";
+    char* username = argv[2];
 
-//     Network n;
-//     Client c;
+    Network n;
+    Client c;
 
-//     signal(SIGINT, cfinish);
-//     signal(SIGTERM, cfinish);
+    signal(SIGINT, cfinish);
+    signal(SIGTERM, cfinish);
 
-//     NewNetwork(&n);
-//     ConnectNetwork(&n, host, port);
-//     MQTTClient(&c, &n, 1000, buf, 100, readbuf, 100);
+    NewNetwork(&n);
+    ConnectNetwork(&n, host, port);
+    MQTTClient(&c, &n, 1000, buf, 100, readbuf, 100);
 
-//     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-//     data.willFlag = 0;
-//     data.MQTTVersion = 3;
-//     data.clientID.cstring = clientid;
-//     data.username.cstring = username;
-//     //data.password.cstring = opts.password;
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.willFlag = 0;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = clientid;
+    data.username.cstring = username;
+    //data.password.cstring = opts.password;
 
-//     data.keepAliveInterval = 10;
-//     data.cleansession = 1;
-//     printf("Connecting to %s %d\n", host, port);
+    data.keepAliveInterval = 10;
+    data.cleansession = 1;
+    printf("Connecting to %s %d\n", host, port);
 
-//     rc = MQTTConnect(&c, &data);
-//     printf("Connected %d\n", rc);
+    rc = MQTTConnect(&c, &data);
+    printf("Connected %d\n", rc);
 
-//     printf("Subscribing to %s\n", topic);
-//     rc = MQTTSubscribe(&c, topic, QOS1, messageArrived);
-//     printf("Subscribed %d\n", rc);
+    printf("Subscribing to %s\n", topic);
+    rc = MQTTSubscribe(&c, topic, QOS1, messageArrived);
+    printf("Subscribed %d\n", rc);
 
-//     while (!toStop)
-//     {
-//         MQTTYield(&c, 1000);
-//     }
+    while (!toStop)
+    {
+        MQTTYield(&c, 1000);
+    }
 
-//     printf("Stopping\n");
+    printf("Stopping\n");
 
-//     MQTTDisconnect(&c);
-//     n.disconnect(&n);
+    MQTTDisconnect(&c);
+    n.disconnect(&n);
 
-//     return 0;
-// }
+    return 0;
+}
